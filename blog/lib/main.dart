137218013495e6060670a908528bc9b851d39ecb
Keyul Jain <keyuljainkj@gmail.com>
2021-07-10 20:16:52 +0530

    :fire: Connected backend

--- blog/lib/main.dart
+++ blog/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:advance_pdf_viewer/advance_pdf_viewer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -170,18 +171,20 @@ class _MyAppState extends State<MyApp> {
                 // crossAxisAlignment: CrossAxisAlignment.baseline,
                 // textBaseline: TextBaseline.alphabetic,
                 children: [
-                  blogCard(200.0),
-                  blogCard(170.0),
-                  blogCard(200.0),
+                  blogCard(200.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(170.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(200.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(170.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
                 ],
               ),
               Column(
                 // crossAxisAlignment: CrossAxisAlignment.baseline,
                 // textBaseline: TextBaseline.alphabetic,
                 children: [
-                  blogCard(170.0),
-                  blogCard(200.0),
-                  blogCard(170.0),
+                  blogCard(170.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(200.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(170.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
+                  blogCard(200.0, Colors.red[400], Colors.red[50], 'Support', 'Contact Us', 'M', Colors.white),
                 ],
               ),
             ],
@@ -211,7 +214,14 @@ class _MyAppState extends State<MyApp> {
 
 class blogCard extends StatelessWidget {
   final height;
-  blogCard(this.height);
+  final inColor;
+  final outColor;
+  final title;
+  final subtitle;
+  final logo;
+  final logoColor;
+
+  blogCard(this.height, this.inColor, this.outColor, this.title, this.subtitle, this.logo, this.logoColor);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -221,7 +231,7 @@ class blogCard extends StatelessWidget {
       child: Container(
         height: 50,
         decoration: BoxDecoration(
-            color: Colors.blue[50],
+            color: this.outColor,
             borderRadius: BorderRadius.all(Radius.circular(20.0))),
         child: new Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -231,23 +241,29 @@ class blogCard extends StatelessWidget {
               height: 50.0,
               width: 50.0,
               decoration: BoxDecoration(
-                  color: Colors.blue[500],
+                  color: this.inColor,
                   borderRadius: BorderRadius.all(Radius.circular(15.0))),
-              child: Icon(
-                Icons.send_sharp,
+              child: Center(
+                child: Text(
+                  this.logo,
+                  style: TextStyle(
+                    color: this.logoColor,
+                    fontSize: 25.0,
+                  ),
+                ),
               ),
             ),
             SizedBox(width: 10),
             Column(
               children: [
                 SizedBox(height: 20),
-                Text("Support",
+                Text(this.title,
                   style: TextStyle(color: Colors.black,
                       fontSize: 17),
                   textAlign: TextAlign.center,),
                 SizedBox(height: 5,),
                 Text(
-                  'Contact Us',
+                  this.subtitle,
                   style: TextStyle(
                     color: Colors.grey,
                     fontSize: 12.0,
@@ -265,25 +281,33 @@ class blogCard extends StatelessWidget {
 }
 
 class SecondRoute extends StatelessWidget {
-  //final doc;
-  //SecondRoute(this.doc);
-  SecondRoute();
+  final doc;
+  SecondRoute(this.doc);
+  //SecondRoute();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Magazine+'),
-      ),
       body: Center(
-        //child: PDFViewer(document: this.doc)
-        child: UserInformation(),
+        child: PDFViewer(document: this.doc)
       ),
     );
   }
 }
 
 class testpdf extends StatelessWidget {
-  const testpdf({Key? key}) : super(key: key);
+
+  Future<String> getUrl(name) async {
+    var ref = firebase_storage.FirebaseStorage.instance.ref().child('Pdf').child('Thumbnails').child(name);
+    var url = await ref.getDownloadURL();
+    return url;
+  }
+
+  Future<String> getPath(name) async {
+    var path = firebase_storage.FirebaseStorage.instance.ref().child('Pdf').child('pdfs').child(name);
+    var url = await path.getDownloadURL();
+    return url;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -305,122 +329,139 @@ class testpdf extends StatelessWidget {
                   decoration: BoxDecoration(
                       color: Colors.blue[50],
                       borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                  // child: new Row(
-                  //   crossAxisAlignment: CrossAxisAlignment.center,
-                  // )
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(30),
-                    child: Image.network('https://www.nasa.gov/sites/default/files/styles/stem_hero/public/thumbnails/image/nasa-logo-web-rgb.jpg', fit: BoxFit.fill),
-                    //fit: BoxFit.fitHeight)
+                    child: FutureBuilder<String>(
+                        future: getUrl('nasa.png'),
+                        builder: (context, AsyncSnapshot<String> snapshot) {
+                          if (snapshot.hasData) {
+                            return Image.network(snapshot.data!, fit: BoxFit.fill);
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        }
+                    )
                   ),
                 ),
                 padding: EdgeInsets.all(10.0),
               ),
               onTap: () async{
-                //PDFDocument doc = await PDFDocument.fromURL('https://www.unf.edu/~wkloster/2220/ppts/cprogramming_tutorial.pdf');
+                var url = await getPath('Experiment 1.pdf');
+                PDFDocument doc = await PDFDocument.fromURL(url);
                 Navigator.push(
                   context,
-                  //MaterialPageRoute(builder: (context) => SecondRoute(doc)),
-                  MaterialPageRoute(builder: (context) => SecondRoute()),
+                  MaterialPageRoute(builder: (context) => SecondRoute(doc)),
                 );
               },
             ),
-            Container(
-              height: 50.0,
-              width: 140.0,
-              color: Colors.transparent,
+            GestureDetector(
               child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Colors.blue.shade50,
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                // child: new Row(
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //
-                // )
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.network('https://www.nasa.gov/sites/default/files/styles/stem_hero/public/thumbnails/image/nasa-logo-web-rgb.jpg', fit: BoxFit.fill),
-                  //fit: BoxFit.fitHeight)
+                height: 50.0,
+                width: 140.0,
+                color: Colors.transparent,
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: FutureBuilder<String>(
+                          future: getUrl('nasa.png'),
+                          builder: (context, AsyncSnapshot<String> snapshot) {
+                            if (snapshot.hasData) {
+                              return Image.network(snapshot.data!, fit: BoxFit.fill);
+                            } else {
+                              return CircularProgressIndicator();
+                            }
+                          }
+                      )
+                  ),
                 ),
+                padding: EdgeInsets.all(10.0),
               ),
-              padding: EdgeInsets.all(10.0),
+              onTap: () async{
+                var url = await getPath('Experiment 1.pdf');
+                PDFDocument doc = await PDFDocument.fromURL(url);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute(doc)),
+                );
+              },
             ),
-            Container(
-              height: 50.0,
-              width: 140.0,
-              color: Colors.transparent,
+            GestureDetector(
               child: Container(
-                height: 50,
-                decoration: BoxDecoration(
-                    color: Colors.blue.shade50,
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                // child: new Row(
-                //   crossAxisAlignment: CrossAxisAlignment.center,
-                //
-                // )
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.network('https://www.nasa.gov/sites/default/files/styles/stem_hero/public/thumbnails/image/nasa-logo-web-rgb.jpg', fit: BoxFit.fill),
-                  //fit: BoxFit.fitHeight)
+                height: 50.0,
+                width: 140.0,
+                color: Colors.transparent,
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: FutureBuilder<String>(
+                          future: getUrl('nasa.png'),
+                          builder: (context, AsyncSnapshot<String> snapshot) {
+                            if (snapshot.hasData) {
+                              return Image.network(snapshot.data!, fit: BoxFit.fill);
+                            } else {
+                              return CircularProgressIndicator();
+                            }
+                          }
+                      )
+                  ),
                 ),
+                padding: EdgeInsets.all(10.0),
               ),
-              padding: EdgeInsets.all(10.0),
+              onTap: () async{
+                var url = await getPath('Experiment 1.pdf');
+                PDFDocument doc = await PDFDocument.fromURL(url);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute(doc)),
+                );
+              },
             ),
-            Container(
-              height: 50.0,
-              width: 140.0,
-              color: Colors.transparent,
+            GestureDetector(
               child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.blue[50],
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: Image.network('https://www.nasa.gov/sites/default/files/styles/stem_hero/public/thumbnails/image/nasa-logo-web-rgb.jpg', fit: BoxFit.fill),
-                  //fit: BoxFit.fitHeight)
+                height: 50.0,
+                width: 140.0,
+                color: Colors.transparent,
+                child: Container(
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: FutureBuilder<String>(
+                          future: getUrl('nasa.png'),
+                          builder: (context, AsyncSnapshot<String> snapshot) {
+                            if (snapshot.hasData) {
+                              return Image.network(snapshot.data!, fit: BoxFit.fill);
+                            } else {
+                              return CircularProgressIndicator();
+                            }
+                          }
+                      )
+                  ),
                 ),
+                padding: EdgeInsets.all(10.0),
               ),
-              padding: EdgeInsets.all(10.0),
+              onTap: () async{
+                var url = await getPath('Experiment 1.pdf');
+                PDFDocument doc = await PDFDocument.fromURL(url);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondRoute(doc)),
+                );
+              },
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-class UserInformation extends StatefulWidget {
-  @override
-  _UserInformationState createState() => _UserInformationState();
-}
-
-class _UserInformationState extends State<UserInformation> {
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('admin').snapshots();
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<QuerySnapshot>(
-      stream: _usersStream,
-      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
-
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
-
-        return new ListView(
-          children: (snapshot.data! as QuerySnapshot).docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
-            return new ListTile(
-              title: new Text(data['img_path']),
-              subtitle: new Text(data['pdf_path']),
-            );
-          }).toList(),
-        );
-      },
     );
   }
 }
